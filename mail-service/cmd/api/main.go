5999a1cad6b1552e756a68ec1242e91cb2d00b0a
Erik Westra <e.s.westra.95@gmail.com>
2023-02-21 14:52:54 +0100

    finish mail service

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Mailer Mail
+}
+
+const webPort = "80"
+
+func main() {
+	app := &Config{
+		Mailer: createMail(),
+	}
+
+	log.Println("Starting mail service on port", webPort)
+
+	srv := &http.Server{
+		Addr:    ":" + webPort,
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createMail() Mail {
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+	}
+	return m
+}
